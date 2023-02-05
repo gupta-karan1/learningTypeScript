551b775bcd082aa65dc60dd2831dd25678b1e582
Karan Gupta <d22124440@mytudublin.ie>
2023-02-05 22:47:25 +0000

    functions

--- 01_intro/myFunction.js
+++ 01_intro/myFunction.js
@@ -1,14 +1,38 @@
 "use strict";
 exports.__esModule = true;
 function addNum(num) {
-    return num + 2;
+    //   return "hello";
+    return num + 4;
 }
 function getUpper(val) {
     return val.toUpperCase();
 }
 function signUpUser(name, email, isPaid) { }
-var loginUser = function (name, email, isPaid) { };
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
 addNum(5);
 getUpper("california");
 signUpUser("karan", "karang6@gmail.com", false);
 loginUser("k", "k@k.com");
+// function getValue(myVal: number): boolean {
+//     if (myVal > 5) {
+//         return true
+//     }
+//     return "200 OK"
+// }
+var getHello = function (s) {
+    return "";
+};
+// const heroes = ["thor", "ironMan", "spiderMan"]
+var heroes = [1, 2, 3];
+heroes.map(function (hero) {
+    return "hero is ".concat(hero);
+    // return 2
+});
+function consoleError(errMsg) {
+    console.log(errMsg);
+}
+function handleError(errMsg) {
+    throw new Error(errMsg);
+}
--- 01_intro/myFunction.ts
+++ 01_intro/myFunction.ts
@@ -1,5 +1,6 @@
-function addNum(num: number) {
-  return num + 2;
+function addNum(num: number): number {
+  //   return "hello";
+  return num + 4;
 }
 
 function getUpper(val: string) {
@@ -15,5 +16,31 @@ getUpper("california");
 
 signUpUser("karan", "karang6@gmail.com", false);
 loginUser("k", "k@k.com");
+
+// function getValue(myVal: number): boolean {
+//     if (myVal > 5) {
+//         return true
+//     }
+//     return "200 OK"
+// }
+
+const getHello = (s: string): string => {
+  return "";
+};
+
+// const heroes = ["thor", "ironMan", "spiderMan"]
+const heroes = [1, 2, 3];
+
+heroes.map((hero): string => {
+  return `hero is ${hero}`;
+  // return 2
+});
+
+function consoleError(errMsg: string): void {
+  console.log(errMsg);
+}
+function handleError(errMsg: string): never {
+  throw new Error(errMsg);
+}
 
 export {};
