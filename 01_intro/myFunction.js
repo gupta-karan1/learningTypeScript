66edc8e14283c295fb13e54cd63b2e8537a36658
Karan Gupta <d22124440@mytudublin.ie>
2023-02-05 22:16:46 +0000

    wip

---
+++ 01_intro/myFunction.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+function addNum(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) { };
+addNum(5);
+getUpper("california");
+signUpUser("karan", "karang6@gmail.com", false);
+loginUser("k", "k@k.com");
