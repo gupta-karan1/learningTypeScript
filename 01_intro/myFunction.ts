8e4c8172266478a2dce690dc89a86700b5509b6d
Karan Gupta <d22124440@mytudublin.ie>
2023-02-05 22:16:41 +0000

    wip

---
+++ 01_intro/myFunction.ts
@@ -0,0 +1,19 @@
+function addNum(num: number) {
+  return num + 2;
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
+
+addNum(5);
+getUpper("california");
+
+signUpUser("karan", "karang6@gmail.com", false);
+loginUser("k", "k@k.com");
+
+export {};
