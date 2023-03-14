9255eb2e147b1e23f35ae35ef5ac9000572bba3e
Karan Gupta <d22124440@mytudublin.ie>
2023-03-14 20:03:01 +0000

    datatype numbers

--- datatypes.js
+++ datatypes.js
@@ -3,3 +3,12 @@ let lname = "Gupta";
 lname = "Singh";
 let newName = lname.toUpperCase();
 console.log(newName);
+let age = 30;
+age = 27;
+age = 27.5;
+let dob = "28";
+let result = parseInt(dob);
+// typescript knows the data type from the value which is called type inference
+console.log(result);
+let isValid = true;
+console.log(isValid);
--- datatypes.ts
+++ datatypes.ts
@@ -2,3 +2,16 @@ let lname: string = "Gupta";
 lname = "Singh";
 let newName = lname.toUpperCase();
 console.log(newName);
+
+let age: number = 30;
+age = 27;
+age = 27.5;
+
+let dob = "28";
+
+let result = parseInt(dob);
+// typescript knows the data type from the value which is called type inference
+console.log(result);
+
+let isValid: boolean = true;
+console.log(isValid);
